7fa272381f86bf1423a1743c27b46758bcbb6908
Abrar Shahriar <abrarshahriar360@gmail.com>
2020-03-21 18:28:37 +0600

    First commit

---
+++ Science_Fair_2020_project/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Science_Fair_2020_project project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Science_Fair_2020_project.settings')
+
+application = get_asgi_application()
